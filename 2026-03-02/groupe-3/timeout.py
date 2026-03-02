771ec59e34545db19a7e6dc40e747a1d7aa5f514
Etienne Lozes <elozes@unice.fr>
2026-03-02 15:12:40 +0100

    2 mars

--- 2026-03-02/groupe-3/timeout.py
+++ 2026-03-02/groupe-3/timeout.py
@@ -1,1 +1,9 @@
-# exercice supplementaire: ecrire un programme timeout.py cmd delay qui lance la commande dans un fils et tue le fils au bout de delay s'il n'a pas encore terminé
+# exercice supplementaire: ecrire un programme `timeout.py cmd delay`
+# qui lance la commande dans un fils et tue le fils au bout de delay 
+# s'il n'a pas encore terminé
+
+# indices: voir photo
+# utiliser signal.alarm pour programmer un minuteur dans le père
+# installer un handler dans le père pour réagir à sigalarm
+# envoyer sigkill au fils depuis le handler
+
